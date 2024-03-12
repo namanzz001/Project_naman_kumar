dbe424e71389e32ecf909951c1acc537a8a8954d
namanzz001 <naman23236@gmail.com>
2024-03-12 18:47:15 +0530

    reactintroassign3commit

---
+++ reactintro/reactintroassign3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header.js'
+import Videocard from './components/videocard.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Videocard />
+    </div>
+  );
+}
+
+export default App;
